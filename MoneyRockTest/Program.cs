41d2122c9b4d748d31e75ef3a79151541fd3b568
Dmitry Machikhelyan <dimaragainst@gmail.com>
2020-12-01 12:59:36 +0300

    first commit

---
+++ MoneyRockTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace MoneyRockTest
+{
+    class Program
+    {
+        public static bool keepRunnig = true;
+        static async Task Main(string[] args)
+        {
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e) {
+                e.Cancel = true;
+                Program.keepRunnig = false;
+            };
+ 
+
+            RedisService redisService = new RedisService();
+            PostgresService postgresService = new PostgresService();
+
+            while(Program.keepRunnig)
+            {
+                var command = Console.ReadLine();
+
+                switch(command)
+                {
+                    case "/redis":
+                        Console.WriteLine("use redis service");
+                        break;
+                    case "/postgres":
+                        
+                        Console.WriteLine(await postgresService.ReadAsync("dad")); 
+                        Console.WriteLine("use postgres service");
+                        break;
+                    case "/rabit":
+                        Console.WriteLine("use rabit servce");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Exited...");
+        }
+
+
+
+    }
+}
---
+++ MoneyRockTest/RedisService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace MoneyRockTest
+{
+    public class RedisService
+    {
+        private readonly ConnectionMultiplexer _redis;
+        private readonly IDatabase _database;
+        public RedisService()
+        {
+            try
+            {
+                _redis = ConnectionMultiplexer.Connect("localhost:6379");
+                _database = _redis.GetDatabase();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void SetString(string key, string value)
+        {
+            var result = _database.StringSet(key, value);
+            if(result)
+                Console.WriteLine($"Добавлено:\n[\n{key}: {value}\n]");
+            else
+                Console.WriteLine("Не добавлено");
+        }
+
+        public void GetString(string key)
+        {
+            var valueOfKey = _database.StringGet(key);
+            Console.WriteLine(valueOfKey.ToString());
+        }
+        
+
+
+    }
+}
